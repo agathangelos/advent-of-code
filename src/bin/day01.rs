4bd86a09fe108146947de56ef18f16ac1e295e88
angelos <agathangelos.stylianidis@gmail.com>
2020-12-15 13:44:05 +0200

    refactor day01

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -3,25 +3,24 @@ use anyhow::{bail, Result};
 
 fn main() -> Result<()> {
     let input = file_lines("./input/day01/input.txt")?;
-    let numbers: Vec<u32> = input.map(|n| n.parse::<u32>().unwrap()).collect();
-    // let mut v = &numbers;
+    let mut numbers: Vec<u32> = input.map(|n| n.parse::<u32>().unwrap()).collect();
+    numbers.sort();
+    let numbers = numbers;
+
+    let sol1 = solve_part1(&numbers, 2020)?;
+    println!("Part one answer: {}", sol1);
 
     let sol2 = solve_part2(&numbers, 2020)?;
     println!("Part two answer: {}", sol2);
 
-    let sol1 = solve_part1(numbers, 2020)?;
-    println!("Part one answer: {}", sol1);
-
     Ok(())
 }
 
-fn solve_part1(mut numbers: Vec<u32>, target: u32) -> Result<u32> {
-    &numbers.sort();
-    let len = numbers.len();
-    for i in 0..len {
-        let ii = (&numbers)[i];
-        if let Some(i) = (&numbers).into_iter().find(|&n| *n == (target - ii)) {
-            return Ok(ii * i);
+fn solve_part1(numbers: &Vec<u32>, target: u32) -> Result<u32> {
+    for i in 0..numbers.len() {
+        numbers.iter().find(|&n| *n == (target - numbers[i]));
+        if let Some(n) = &numbers.iter().find(|&n| *n == (target - numbers[i])) {
+            return Ok(numbers[i] * *n);
         }
     }
 
